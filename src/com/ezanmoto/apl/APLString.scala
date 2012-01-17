59f13cc7e00c6642527dde11c9370cd8140b629b
Sean Kelleher <ezanmoto@gmail.com>
2012-01-17 00:04:52 +0000

    Added multiple indexed string replace

--- src/com/ezanmoto/apl/APLString.scala
+++ src/com/ezanmoto/apl/APLString.scala
@@ -47,16 +47,40 @@ class APLString( private val string: String ) extends Variable {
 
   def replace( index: Variable, value: Variable ) = index match {
     case APLInteger( i ) => this.replace( i, value )
-    case APLList( l ) => throw new RuntimeException( "Not implemented yet" )
+    case APLList( l ) => this.replace( l, value )
     case v => throw new RuntimeException( "Can't use '" + v + "' as an index" )
   }
 
-  private def replace( i: Int, value: Variable ) = value match {
+  private def replace( i: Int, value: Variable ): APLString =
+  value match {
     case APLString( s ) =>
       if ( i > 0 && i <= string.length )
         Variable( string.substring( 0, i - 1 ) + s + string.substring( i ) )
       else
         throw new RuntimeException( "'" + i + "' ! [1.." + string.length + "]" )
+    case v => throw new RuntimeException( "Can't replace chr with '" + v + "'" )
+  }
+
+  private def replace( indices: List[Int], value: Variable ): APLString =
+  value match {
+    case APLString( characters ) =>
+      if ( characters.length == 1 ) {
+        // TODO ensure this is the correct behaviour
+        var c = Variable( characters )
+        var result = this
+        for ( i <- indices )
+          result = result replace( i, c )
+        result
+      } else if ( indices.length == characters.length ) {
+        var result = this
+        for ( i <- 0 to ( indices.length - 1 ) ) {
+          var c = Variable( characters substring ( i, i + 1 ) )
+          result = result replace( indices( i ), c )
+        }
+        result
+      } else
+        throw new RuntimeException( "Number of indices does not match number "
+                                  + "of replacements" )
     case v => throw new RuntimeException( "Can't replace chr with '" + v + "'" )
   }
 
